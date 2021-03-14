fa105d80712c3b5aa81b7fa415b0bf3fc088fa33
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2021-03-14 04:29:28 +0300

    Add files via upload

---
+++ test2.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+my @arr = (3,"123",5);
+print "arr = @arr\n";
+$" = ';';
+print "arr = @arr\n";
+print "1st = $arr[0]\n";
+my @simple = qw(a b c); # == ('a', 'b', 'c')
+my $arref = [1,2,3,4,5]; # = анонимный массив
+
+my $arref1 = [@arr]; # происходит копирование массива и создание анонимного массива
+my $arref2= [(@arr)]; # сначала пытается сделать в список, а потмо из списка в анонимны массив
+my $arref3 = \@arr; # реальная ссылка на массив (ссылка на массив - скаляр)
+print "index of arr\'s last element = ",$#arr,"\n";
+# тот же самый результат в виде: $#$arref3
+print "arr\'s length = ",scalar @arr,"\n";
+# тот же самый результат в виде: scalar @{$arref3}
+my $element = $arref3->[2]; # разыменование ссылки 
+$element = ${$arref3}[2]; # аналогичное разыменование ссылки
+my @array;
+$array[0]=1;
+$array[1] = 2;
+$array[7] = 7;
+# print "4th elem = $array[3]\n"; -> вылетит ошибка, т.к. после [1] до [7] лежит undef
+use Data::Dumper;
+print Dumper \@array,"\n"; # красивый вывод массива, для дебага н-р.
+
+@array = (4,8,15,16,23,42);
+
+print join ",", @array[0,2,4],"\n"; # будет напечатан срез через запятую
+@array[5..7] = (10,20,30); # снова срез с присваиванием
+
+my %first = ( # Ключ - ВСЕГДА СТРОКА, значение - любой скаляр
+    key1 => "value1", # => - жИрНаЯ запятая
+    key2 => "value2",
+);
+=my %second - ( # в предыдущем хеше перл сам привел ключ к строке
+    'key3',"value3",
+    'key4', "value4",
+);
+=cut
+
+my %third = qw(key5 value5 key6 value6);
+my %all = (%first, %second, %third); # это не список хешей, а новый хеш, но просто проиниц. списком хешей
+
+my $href = \%hash; # ссылка на хеш
+
+my $href = { # анонимный хеш, ссылка на хеш
+    key1 => "value1",
+    key2 => "value2",
+};
+
+my @array  = qw(key1 val1 key2 val2);
+my %hash = @array; # хеш можно инициализировать массивом
+
+my $aref = [qw(key1 val1 key2 val2)];
+my %hash = @{$aref}; # инициализация ссылкой (происх. копирование)
+
+my $hashref = {@array};
+my $hashref = {@{$aref}};
+
+# BAREWORD - строчка без кавычек
+# бареворды можно юзать в хешах
+
+my %hash = (key1 => "val1");
+print "${hash{key2}}\n";
